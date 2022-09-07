b46c78c206e4fb573166388bf40c49c1795fbd76
Rhesa Tendean <rhesa.tendean@gmail.com>
2022-09-07 22:46:34 +0200

    Updated layout

--- Shiny_Layout.R
+++ Shiny_Layout.R
@@ -1,9 +1,13 @@
-# Check libraries
+#Sichern, dass StringsAsFactors gleich FALSE
+options(stringsAsFactors = F)
 
+# Überprüfen, ob die Packages bereits installiert sin
 if(!require(readr)){
+  # wenn es nicht installiert ist, wird es nun installiert
   install.packages("readr", dependencies = TRUE)
   library(readr)
 }
+
 
 if(!require("tidyverse")){
   install.packages("tidyverse")
@@ -24,53 +28,60 @@ if(!require("modelr")){
   library(modelr)
 }
 
+# HEAD
 if(!require("lubridate")){
   install.packages("lubridate")
   library(lubridate)
 }
 
-if(!require("shinythemes")){
-  install.packages("shinythemes")
-  library(shinythemes)
-}
-
-if(!require("shinydashboard")){
-  install.packages("shinydashboard")
-  library(shinydashboard)
-}
+final_dataset <- read_delim("Final_dataset_group_07.csv",delim = ",")
 
 ui <- fluidPage(
   theme = shinytheme("cerulean"),
-
-  titlePanel(div(column(width = 5, h2("IDA Study Case Group 07")),
-                 column(width = 1, tags$img(src = "qw_gross_trimmed.png", height = 40, width = 150))),
+  
+  titlePanel(div(column(width = 7, h2("IDA Study Case Group 07")),
+                 column(width = 1, tags$img(src = "qw_gross_trimmed.png", height = 70, width = 125))),
              windowTitle="TitlePage"),
+  
   mainPanel(
     
-    tabsetPanel(type = "tabs",
-                tabPanel("Summary", verbatimTextOutput("summary")),
-                tabPanel("View", tableOutput("view")),
-                tabPanel("Plot",
-                         fluidRow(
-                           sidebarLayout(
-                             sidebarPanel(
-                               selectInput("Gemeinden","Gemeinden: ",c(""),multiple = TRUE),
-                               dateRangeInput("daterange","Date range:",start = "2014-01-01",end="2016-12-30",min = "2014-01-01",max="2016-12-30")
-                             ),
-                             mainPanel(
-                               plotOutput(outputId ="plot")
-                             ),
-                             position = "left")
-                         )
-                )
+    tabsetPanel(
+      type = "tabs",
+      tabPanel("Summary", verbatimTextOutput("summary")),
+      tabPanel("View", 
+               fluidRow(
+                 numericInput(inputId = "obs",
+                              label = "Number of observations to view:",
+                              value = 10)),
+               fluidRow(
+                 tableOutput(outputId = "view")),
+      ),
+      
+      tabPanel("Plot",
+               fluidRow(
+                 sidebarLayout(
+                   sidebarPanel(
+                     selectInput("Gemeinden","Gemeinden: ",c(""),multiple = TRUE),
+                     dateRangeInput("daterange","Date range:",
+                                    start = min(final_dataset$Zulassung),
+                                    end=max(final_dataset$Zulassung),
+                                    min = min(final_dataset$Zulassung),
+                                    max=max(final_dataset$Zulassung))
+                   ),
+                   mainPanel(
+                     plotOutput(outputId ="plot")
+                   ),
+                   position = "left")
+               )
+      )
     )
   )
-
   
 )
 
+
 server <- function(input,output){
-  
+
 }
 
 shinyApp(ui = ui, server = server)
